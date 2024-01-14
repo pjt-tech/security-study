1ad704f6df78b57d19051ea1f4a4fb7130ae9023
pjt-tech <bagjintae@bagjintaeui-MacBookPro.local>
2024-01-14 17:43:56 +0900

    init

---
+++ src/main/java/com/mac/test/TestApplication.java
@@ -0,0 +1,13 @@
+package com.mac.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestApplication {
+	// security test
+	public static void main(String[] args) {
+		SpringApplication.run(TestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mac/test/TestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mac.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
